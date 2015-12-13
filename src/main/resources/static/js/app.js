7075dc9476a02f1efa845254b51c36c465a514d9
dsw9742 <dsw9742@hotmail.com>
2015-12-13 15:24:54 -0500

    Adding comments to app.js for TMS-related functionality.

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -30,18 +30,30 @@ angular.module('DataService', []) // service to retrieve data from server-side a
 		
 	  // option 1
 	  $(document).trigger("digitalDataRefresh", {id:"0", name:"digitalDataRefresh"}); // fire custom digitalDataRefresh event using jQuery. This can be observed by tag management 
-		                                                                  			  // system using Delegate / On Ensighten Framework or jQuery On method. This 
-		                                                                  			  // is probably the most flexible option.
+		                                                                  			  // system using the jQuery .on() method. This is probably the most flexible option since 
+	                                                                                  // jQuery's .on() method is more extensive than Ensighten Events or the Ensighten Framework 
+	                                                                                  // Delegation / On Bootstrapper.on() method. Consider using this if option if your TMS users 
+	                                                                                  // are comfortable wrapping tags with jQuery code and you can use the jQuery library.
 		
 	  // option 2
-	  //Bootstrapper.ensEvent.trigger("digitalDataRefresh", {id:"0", name:"digitalDataRefresh"}); // fire custom digitalDataRefresh Ensighten event using 
-		                                                                              			  // Ensighten's proprietary event code. This can be observed by tag
-		                                                                              			  // management system using Delegate / On Ensighten Framework or jQuery 
-		                                                                              			  // On method. This is probably the most flexible option.
+	  Bootstrapper.ensEvent.trigger("digitalDataRefresh"); // fire custom digitalDataRefresh Ensighten event using Ensighten's proprietary Bootstrapper.ensEvent.trigger() method. 
+	                                                       // This can be observed by tag management system by using Ensighten's Events:Named Events feature. Once Events are
+	                                                       // created that correspond with the event names implemented by the development team, TMS users can use those Events as
+	                                                       // Conditions when deploying tag code instead of requiring them to manually wrap their tags with code. Supposedly this
+	                                                       // option also allows tag code to be wrapped by Boostrapper.on() method (Ensighten Framework Delegation / On) but I
+	                                                       // personally have not seen this work with AngularJS.
 		
 	  // option 3
-	  //window.digitalDataLastUpdate = new Date(); // update digitalDataLastUpdate variable. This variable can be watched by the tag management system
-		                                           // using Events:Value Changes type. This option polls.
+	  //window.digitalDataLastUpdate = new Date(); // update digitalDataLastUpdate variable. This variable can be watched by the tag management system using Ensighten's Events:
+	                                               // Value Changes feature. This feature polls so there are performance implications to consider, but it does also allow TMS users
+	                                               // to use Events as Conditions when deploying tag code instead of requiring them to manually wrap their tags with code.
+	  
+	  // option 4
+	  //Ensighten Framework Property Watcher Helper // more info is available at https://success.ensighten.com/hc/en-us/articles/201720700-Helpers-Property-Watcher. This option 
+	                                                // is not built out as it is very similar to option 3 but Bootstrapper.propertyWatcher is attached to window.digitalDataLastUpdate,
+	                                                // configured, and polls for changes. Either the tag code can be deployed directly in response to the change as part of the 
+	                                                // Bootstrapper.propertyWatcher configuration, or a custom event can be triggered and observed by jQuery or Ensighten.
+	  
 	};
     return Data;
   }]);
